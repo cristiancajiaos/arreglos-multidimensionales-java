c0cb0eb54c0e670052cc7a3b663575705f5b4aaa
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-05-31 15:21:18 -0400

    Agregada implementación

--- src/arreglos/multidimensionales/ArreglosMultidimensionales.java
+++ src/arreglos/multidimensionales/ArreglosMultidimensionales.java
@@ -1,20 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package arreglos.multidimensionales;
 
-/**
- *
- * @author crist
- */
 public class ArreglosMultidimensionales {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Matriz de ceros 
+        /*
+        int[][] numeros = new int[3][3];
+        int i, j; // i = filas, j = columnas
+        
+        // Recorrer filas
+        for (i = 0; i < numeros.length; i++) {
+            System.out.println();
+            // Recorrer columnas
+            for (j = 0; j < numeros.length; j++) {
+                System.out.print(numeros[i][j] + " ");
+            }
+        }
+        */
+        
+        int[][] matriz = new int[4][4];
+        matriz[0][0] = 4;
+        matriz[0][1] = 3;
+        matriz[1][0] = 7;
+        matriz[1][1] = 6;
+        int fila, columna; 
+        
+        // Recorrer filas
+        for (fila = 0; fila < matriz.length; fila++) {
+            System.out.println("");
+            for (columna = 0; columna < matriz.length; columna++) {
+                System.out.print(matriz[fila][columna] + " ");
+            }
+        }
+
     }
-    
+
 }
